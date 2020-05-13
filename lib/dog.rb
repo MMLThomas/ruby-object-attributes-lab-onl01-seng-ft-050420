946ec163cd2347cfcb31c336ac7240c643a38df3
michael thomas <michael.thomas2014@gmail.com>
2020-05-13 03:13:35 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def name=(dogs_name)
+    @var_dogs_name = dogs_name
+  end
+  def name
+    @var_dogs_name
+  end
+  def breed=(dogs_breed)
+    @var_dogs_breed = dogs_breed
+  end
+  def breed 
+    @var_dogs_breed
+  end
+end
+    
