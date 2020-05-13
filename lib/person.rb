4e25dcb06e155fbae5bcf8b379dca796c2c61f26
michael thomas <michael.thomas2014@gmail.com>
2020-05-13 03:19:32 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,14 @@
 class Person
   def name=(persons_name)
-    @var_persons_name = persons_name
+    @name = persons_name
   end
   def name
-    @var_persons_name
+    @name
   end
   def job=(persons_job)
-    @var_persons_job = persons_job
+    @job = persons_job
   end
   def job
-    @var_persons_job
+    @job
   end
 end
